ef0416f539b0ad5b9b95a9e621908005726c850e
mattgd <mattdzwonczyk@gmail.com>
2016-11-19 16:02:04 -0500

    Level 6 work.

--- src/edu/ncsu/feddgame/GameInstance.java
+++ src/edu/ncsu/feddgame/GameInstance.java
@@ -34,6 +34,7 @@ import edu.ncsu.feddgame.level.Level2;
 import edu.ncsu.feddgame.level.Level3;
 import edu.ncsu.feddgame.level.Level4;
 import edu.ncsu.feddgame.level.Level5;
+import edu.ncsu.feddgame.level.Level6;
 import edu.ncsu.feddgame.level.MainMenu;
 import edu.ncsu.feddgame.level.OptionsMenu;
 import edu.ncsu.feddgame.level.Scene;
@@ -66,6 +67,7 @@ public class GameInstance {
 		add(new Level3());
 		add(new Level4());
 		add(new Level5());
+		add(new Level6());
 		add(new Level10());
 		add(new GameComplete());
 	}};
@@ -142,7 +144,7 @@ public class GameInstance {
 		double unprocessed = 0;
 		
 		setState(State.GAME); // Set the game state
-		setLevel(6); // Set the starting level
+		setLevel(0); // Set the starting level
 		
 		new Thread(() -> logicLoop()).start(); // Run the logic in a separate thread
 		
---
+++ src/edu/ncsu/feddgame/level/Level6.java
@@ -0,0 +1,78 @@
+package edu.ncsu.feddgame.level;
+
+import edu.ncsu.feddgame.render.CreateModel;
+import edu.ncsu.feddgame.render.LaserStart;
+import edu.ncsu.feddgame.render.Model;
+
+public class Level6 extends Level {
+
+	public Level6() {
+		super("Level 6");
+	}
+	
+	@Override
+	public void renderObjects() {
+		super.renderObjects();
+		// Walls
+		{
+			// Outer bounds
+			CreateModel.createWall(0f, 10f, 20f, .5f);
+			CreateModel.createWall(0f, -10f, 20f, .5f);
+			CreateModel.createWall(-10f, 0f, .5f, 20f);
+			CreateModel.createWall(10f, 0f, .5f, 20f);
+			
+			// Inner bounds
+			CreateModel.createWall(0f, -5f, 8f, .25f);
+			
+			// Around LaserStart
+			CreateModel.createWall(-1f, -1f, .25f, 3f);
+			CreateModel.createWall(1f, -1f, .25f, 3f);
+			CreateModel.createWall(0f, 0.6f, 2.25f, .25f);
+			
+			// Around Laser Stop
+			CreateModel.createWall(-5f, 9f, .25f, 1.5f);
+			CreateModel.createWall(-3f, 9f, .25f, 1.5f);
+			CreateModel.createWall(-7.5f, 10f, 4.8f, 3.5f);
+			CreateModel.createWall(3.5f, 10f, 12.8f, 3.5f);
+			
+			// Others
+			CreateModel.createWall(-4f, 0f, .25f, 5f);
+			CreateModel.createWall(4f, -2.625f, .25f, 5f);
+			CreateModel.createWall(-6f, -2.625f, .25f, 5f);
+			CreateModel.createWall(-4f, 5f, 5f, .25f);
+			CreateModel.createWall(4f, 5f, 5f, .25f);
+		}
+		
+		// Laser start/stop
+		LaserStart laserStart = CreateModel.createLaserStart(0f, 0f, 3);
+		laserWrappers.add(laserStart);
+		CreateModel.createLaserStop(-4f, 9f);
+		
+		// Moveables
+		CreateModel.createMovableBox(4.05f, -5.925f);
+		CreateModel.createMovableTrapezoid(-4f, 5f, 1.5f, 1f, 1f);
+		CreateModel.createMovableTrapezoid(-4f, 8f, 1.5f, 1, 1f);
+		
+		Model triangle = CreateModel.createMovableTriangle(4f, 5f, 1f, 1f);;
+		triangle.rotate((float) Math.toRadians(35));
+		
+		triangle = CreateModel.createMovableTriangle(4f, 6f, 1f, 1f);;
+		triangle.rotate((float) Math.toRadians(90));
+		
+		CreateModel.createMovableTriangle(4f, 5f, 1f, 1f);
+		CreateModel.createMovableTriangle(0f, 5f, 1f, 1f);
+		
+		// Stationary Models
+		triangle = CreateModel.createTriangle(-8.8f, 7.25f, 2f, 2f);
+		triangle.rotate((float) Math.toRadians(90));
+		
+		triangle = CreateModel.createTriangle(-5.38f, 7.75f, 1f, 1f);
+		triangle = CreateModel.createTriangle(8.8f, 7.25f, 2f, 2f);
+	}
+	
+	@Override
+	public void logicLoop() {
+		super.logicLoop();
+	}
+
+}
